c4527882f95b85c2bb70a056941f26c3a93109d3
Stefano Devoti <stefano.devoti92@gmail.com>
2023-05-24 18:40:54 +0200

    "Exercise 3"

--- src/main/java/com/example/springboot/Test3.java
+++ src/main/java/com/example/springboot/Test3.java
@@ -1,54 +1,97 @@
 package com.example.springboot;
 
 import com.example.springboot.model.Meal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class Test3 {
-    private final List<Meal> listaDiPiatti = Arrays.asList(
-            new Meal("LasagneBolognese", "Pasta sfoglia alternata con ragù e besciamella", 10, false),
-            new Meal("SpaghettiAllaCarbonara", "Spaghetti con crema di tuorlo d'uovo, pecorino e guanciale", 8, false),
-            new Meal("BisteccaAllaFiorentina", "", 35, true)
-    );
-    @GetMapping("/meals")
+    private List<Meal> listaDiPiatti = new ArrayList<>();
+
+    //TASK 3
+    // esercizio 1
+    @GetMapping("get/meals")
     public List<Meal> getMeal() {
         return listaDiPiatti;
     }
-    @GetMapping("/meal/{name}")
+    // esercizio 2
+    @GetMapping("get/meal/{name}")
     public Meal getMealByName(@PathVariable("name") String name) {
         for (Meal meal : listaDiPiatti) {
-            if (meal.getName().equals(name)){
+            if (meal.getName().equals(name)) {
                 return meal;
             }
         }
         return null;
     }
-
-    @GetMapping("/meal/description-match/{phrase}")
+    //esercizio 3
+    @GetMapping("get/meal/description-match/{phrase}")
     public Meal getMealByDesc(@PathVariable("phrase") String description) {
-        for (Meal meal : listaDiPiatti){
-            if (meal.getDescription().equalsIgnoreCase(description)){
+        for (Meal meal : listaDiPiatti) {
+            if (meal.getDescription().equalsIgnoreCase(description)) {
                 return meal;
             }
         }
         return null;
     }
-
-    @GetMapping("/meal/price/")
+    //esercizio 4
+    @GetMapping("get/meal/price/")
     public List<Meal> getMealByPriceRange(@RequestParam("min") double minPrice, @RequestParam("max") double maxPrice) {
         List<Meal> listaPiattiRange = new ArrayList<>();
-        for (Meal meal : listaDiPiatti){
-            if (meal.getPrice()>= minPrice && meal.getPrice() < maxPrice){
+        for (Meal meal : listaDiPiatti) {
+            if (meal.getPrice() >= minPrice && meal.getPrice() < maxPrice) {
                 listaPiattiRange.add(meal);
             }
         }
         return listaPiattiRange;
     }
+
+    //TASK 4
+    //esercizio 1
+    @PutMapping("put/meal")
+    public String putMeal(@RequestBody Meal meal) {
+        this.listaDiPiatti.add(meal);
+        return "Piatto aggiunto";
+    }
+    //esercizio 2
+    @PostMapping("/post/meal/{name}")
+    public String modifyMealByName(@PathVariable("name") String name, @RequestBody Meal updatedMeal) {
+        for (Meal meal : listaDiPiatti) {
+            if (meal.getName().equals(name)) {
+                meal.setName(updatedMeal.getName());
+                meal.setDescription(updatedMeal.getDescription());
+                meal.setIngredients(updatedMeal.getIngredients());
+                return "Piatto modificato";
+            }
+        }
+        return null;
+    }
+    //esercizio 3
+    @DeleteMapping("delete/meal/{name}")
+    public String deleteMeal(@PathVariable("name") String name) {
+        this.listaDiPiatti.removeIf(meal -> meal.getName().equals(name));
+        return "Piatto cancellato";
+    }
+    //esercizio 4
+    @DeleteMapping("delete/meal/price/{price}")
+    public String deleteMealAbovePrice(@PathVariable("price") double price) {
+        listaDiPiatti.removeIf(meal -> meal.getPrice() > price);
+        return "Piatti che superano il prezzo stabilito cancellati";
+    }
+    //esercizio 5
+    @PutMapping("put/meal/{name}/price")
+    public String putPriceByName(@PathVariable("name") String name, @RequestBody Meal updatedPrice) {
+        for (Meal meal : listaDiPiatti) {
+            if (meal.getName().equals(name)) {
+                meal.setPrice(updatedPrice.getPrice());
+                return "Piatto modificato";
+            }
+        }
+        return null;
+    }
+
+
 }
