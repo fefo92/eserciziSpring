217a6aaafee877bb2bf9f3e812a9d8e4de2ad7e0
Stefano Devoti <stefano.devoti92@gmail.com>
2023-05-31 17:13:33 +0200

    "Exercise 4"

--- src/main/java/com/example/springboot/controller/IngredientController.java
+++ src/main/java/com/example/springboot/controller/IngredientController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 public class IngredientController {
@@ -21,9 +20,39 @@ public class IngredientController {
         this.mealService = mealService;
     }
 
-    @PutMapping("/add-ingredient")
-    public ResponseEntity<Meal> addMeal() {
+    @PostMapping("/add-ingredient")
+    public ResponseEntity<String> addIngredient(@RequestBody Ingredient ingredient) {
+        ingredientService.addIngredient(ingredient);
+
+        return ResponseEntity.ok("Ingrediente creato");
+
+    }
+    @GetMapping("/get-ingredient/{id}")
+    public ResponseEntity<Ingredient> getIngredientById(@PathVariable Long id){
+        ingredientService.getIngredientById(id);
 
         return ResponseEntity.ok().build();
     }
+    @DeleteMapping("/delete-ingredient/{id}")
+    public ResponseEntity<String> deleteIngredientById(@PathVariable Long id){
+        ingredientService.removeIngredientById(id);
+
+        return ResponseEntity.ok("Ingrediente rimosso");
+    }
+    @PutMapping("/put-ingredient/{id}")
+    public ResponseEntity<String> modifyIngredient(@PathVariable Long id, @RequestBody Ingredient ingredient){
+        ingredientService.removeIngredientById(id);
+        ingredientService.addIngredient(ingredient);
+
+        return ResponseEntity.ok("Ingrediente modificato");
+
+    }
 }
+
+//CONTROLLER oggetti che ricevono le api e le elaborano, parlano con il frontend. Posso implementare degli endpoint e
+//delle operazioni crud ma non hanno logica al loro interno (come specie un interfaccia), è associato 1 per ogni entità
+
+//la logica dei metodi sta nel SERVICE, ma non per tutte le entità, solo per quelle che hanno bisogno di delle funzionalità.
+//un service può essere utilizzato da più controllers
+
+//@repository sono interfacce che contengono una serie di metodi crud per gestire il DB(crea automaticamente lo script sql)
--- src/main/java/com/example/springboot/service/IngredientService.java
+++ src/main/java/com/example/springboot/service/IngredientService.java
@@ -5,6 +5,8 @@ import com.example.springboot.model.Ingredient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class IngredientService  {
     private IngredientDao ingredientDao;
@@ -16,5 +18,35 @@ public class IngredientService  {
 
     public void addIngredient(Ingredient dumpling) {
         ingredientDao.save(dumpling);
+    }
+
+    public void addIngredients(Iterable<Ingredient> ingredients) {
+        ingredientDao.saveAll(ingredients);
+    }
+
+    public Ingredient getIngredientById(Long id) {
+        //contenitore che può sia contenere l'entità che non contenerla
+        Optional<Ingredient> optionalIngredient = ingredientDao.findById(id);
+        if(optionalIngredient.isPresent()){
+            return optionalIngredient.get();
+        }
+        throw new NullPointerException();
+
+    }
+
+    public Iterable<Ingredient> getAllIngredientById(Iterable<Long> idList) {
+        return ingredientDao.findAllById(idList);
+    }
+
+    public void removeIngredientById(Long id) {
+        ingredientDao.deleteById(id);
+    }
+
+    public Ingredient removeAllIngredientsById(Long id){
+        Optional<Ingredient> optionalIngredient = ingredientDao.findById(id);
+        if(optionalIngredient.isPresent()){
+            return optionalIngredient.get();
+        }
+        throw new NullPointerException();
     }
 }
